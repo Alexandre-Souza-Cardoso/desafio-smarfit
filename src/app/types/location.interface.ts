0cb66160811b2a5e984f047adae1578dd3dedc5b
alexandre cardoso <alexandre.cardoso@redes.unb.br>
2024-11-04 16:13:34 -0300

    feat: create get unit service

---
+++ src/app/types/location.interface.ts
@@ -0,0 +1,19 @@
+import { Scheduler } from "timers/promises";
+
+export interface Location {
+    id: number,
+    title: string,
+    content: string,
+    opened: boolean,
+    mask: string,
+    towel: string,
+    foutain: string,
+    locker_room: string,
+    schedules: Schedule[]
+}
+
+interface Schedule {
+    weekdays: string,
+    hour: string
+}
+
