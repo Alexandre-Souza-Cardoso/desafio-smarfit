6df7b92d0648f995b23149f5f92c475e179526de
Alexandre <ale.developer72@gmail.com>
2025-08-24 16:24:32 -0300

    feat: moved filter and card list to get unit service

--- src/app/components/cards-list/cards-list.component.ts
+++ src/app/components/cards-list/cards-list.component.ts
@@ -1,16 +1,18 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { Location } from '../../types/location.interface';
+import { GetUnitsService } from '../../services/get-units.service';
 
 
 @Component({
   selector: 'app-cards-list',
   templateUrl: './cards-list.component.html',
-  styleUrl: './cards-list.component.scss'
+  styleUrls: ['./cards-list.component.scss']
 })
 export class CardsListComponent implements OnInit {
   @Input() unitsList: Location[] = [];
 
   constructor() {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+  }
 }
